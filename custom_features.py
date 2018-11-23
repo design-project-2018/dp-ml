b8bd5fe3c699e335c720130a6a59584ecc469847
frank-ye <frank.jing.ye@gmail.com>
2018-11-23 01:18:08 -0500

    Finalized Data Preprocessing Code

--- custom_features.py
+++ custom_features.py
@@ -8,36 +8,33 @@ from VGGModel import VGGModel
 
 # Script that creates training data for RNN with dynamic spatial attention model, stored as .npz files
 
-embeddings_path_train = './dataset/custom_features/training'
-embeddings_path_test = './dataset/custom_features/testing'
+
 
 ''' Given a frame and list of obj coordinates, crop objects in frame, then extracts object features ''' 
 def crop_object_features(frame, coord_list, network):
     obj_list = []
 
     # Append full frame first
+    frame = cv2.resize(frame, (224, 224))
     full_frame_feat = network.extract_feature(frame)
     obj_list.append(full_frame_feat)
 
-    if (len(coord_list) < 9):
+    if (len(coord_list) <= 9):
         for i in range(0, len(coord_list)):
             cropped = frame[coord_list[i]['y']:coord_list[i]['y']+coord_list[i]['height'],coord_list[i]['x']:coord_list[i]['x']+coord_list[i]['width']]
             obj_feat = network.extract_feature(cropped)
-            print(obj_feat.shape)
             obj_list.append(obj_feat)
         
         # If less than 10 objects detected
         for j in range(len(coord_list), 9):
             obj_feat = np.zeros_like(obj_list[len(coord_list)])
             obj_list.append(obj_feat)
-            print(obj_feat.shape)
-        print('Length of obj list: {} '.format(len(obj_list)))
     else:
-        for i in range(0, 10):
+        for i in range(0, 9):
             cropped = frame[coord_list[i]['y']:coord_list[i]['y']+coord_list[i]['height'],coord_list[i]['x']:coord_list[i]['x']+coord_list[i]['width']]
             obj_feat = network.extract_feature(cropped)
             obj_list.append(obj_feat)
-        print('Length of obj list: {} '.format(len(obj_list)))
+            print("Object list length: {}".format(len(obj_list)))
 
     return np.asarray(obj_list)
 
@@ -50,37 +47,80 @@ def objects_from_clip(vid_path, json_path, network, n_frames=100):
         json_clip = json.load(f)
     cap = cv2.VideoCapture(vid_path)
     
+    label = json_clip["label"]
     object_frames = []
     ctr= 1
     while (ctr<= n_frames):
+        print("Frame: {}".format(ctr))
         _, frame = cap.read()
         obj_coords = json_clip["frames"][ctr-1]["objects"]
-        print("Cropping feature from frame : {}".format(ctr))
         frame_objects = crop_object_features(frame, obj_coords, network)
-        print("Cropped : {} !".format(ctr))
         object_frames.append(frame_objects)
         ctr +=1
 
-    return np.asarray(object_frames)
+    return np.asarray(object_frames), label
+
+
+
+''' Given path of JSON files with extracted features and labels, loops through files and writes as .npz '''
+def write_batch_directory(inputPath, writePath, extraction_network):
+
+    dTypes = ['training/', 'testing/'] # dataset folder types
+    vTypes = ['positive/', 'negative/'] # accident/no accident folders
+
+    for dType in dTypes:
+        for vType in vTypes:
+            jsonFolder = inputPath + dType + vType
+            vidFolder = './dataset/videos/{}{}'.format(dType, vType)
+            fileNames = os.listdir(jsonFolder)
+            
+            numFiles = len(fileNames)
+            idx_file = 0
+            idx_batch = 1
+            
+            # Need 10 files per batch
+            while (numFiles >= 10):
+                batch_features = []
+                batch_labels = []
+                batchCount = 1
+                # Iterate through files until batchsize of 10 is reached
+                while (batchCount <= 10): 
+                    fileName = fileNames[idx_file]
+                    jsonPath = jsonFolder + fileName
+                    videoName = fileName.split('.json')[0] + '.mp4' # Corresponding video to JSON
+                    vidPath = vidFolder + videoName
+                    
+                    print("Batch Number {} - ".format(idx_batch))
+                    print("Sample Number: {}".format(batchCount))
+                    print("Extracting high level features from video: {}...".format(vidPath))
+                    
+                    feature_array, label = objects_from_clip(vidPath, jsonPath, extraction_network)
+                    
+                    batch_features.append(feature_array)
+                    batch_labels.append(label)
+                    batchCount += 1 # Update sample size of batch
+                    numFiles -= 1 # Update number of videos left to parse
+                    print("Files left to parse: {} ...".format(numFiles))
+                    idx_file += 1 # Update index for parsing through files in folder
+                    
+                # Write to batch file
+                batch_features = np.asarray(batch_features)
+                batch_labels = np.asarray(batch_labels)
+                print("Writing to batch file: {}".format(str(idx_batch).zfill(3)))
+                np.savez_compressed(writePath + 'batch_'+ str(idx_batch).zfill(3) + '.npz', data=batch_features, labels=batch_labels)
+                idx_batch += 1
+                
+    return
+                    
 
 
 def main():
-    # VGG_network = VGGModel()
-    # json_path = './dataset/object_extraction/testing/negative/000830.json'
-    # vid_path = './dataset/videos/testing/negative/000830.mp4'
-    # print("Extracting high level features from {}".format(vid_path))
-    # objects_830 = objects_from_clip(vid_path, json_path, VGG_network)
-    # print(objects_830.shape)
-    # objects_830, alphas_830 = objects_from_clip(vid_path, json_path)
-    # print(objects_830.shape)
-    # test_list = [objects_830, alphas_830]
-    # np.savez_compressed(embeddings_path+ 'test.npz', objects_830, alphas_830) 
-    # opened_arrays = np.load(embeddings_path+'test.npz')
-    # print (opened_arrays.files)
-    # print(opened_arrays['arr_0'].shape)
-    print("hello")
+    VGG_network = VGGModel()
+    json_path = './dataset/object_extraction/'
+    embeddings_path = './dataset/custom_features/'
 
-
+    write_batch_directory(json_path, embeddings_path, VGG_network)
+    # vid_path = './dataset/videos/training/positive/00000001.mp4'
 
 if __name__ == "__main__":
     main()
